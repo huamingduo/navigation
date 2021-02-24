4266902349cf4b704572181ef85b4bcdd7f50034
songby <songby@infore.com>
2021-02-24 21:32:28 +0800

    [amcl] two main changes: * refactor amcl_node.cpp * add correlative_scan_matcher

--- amcl/src/amcl.cc
+++ amcl/src/amcl.cc
@@ -1,353 +1,13 @@
-/*
- *  Copyright (c) 2008, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+#include "amcl.h"
+#include "cartographer/io/probability_grid_points_processor.h"
 
-/* Author: Brian Gerkey */
+using namespace cartographer;
+// using namespace cartographer::mapping;
+using namespace cartographer::mapping::scan_matching;
 
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <cmath>
-#include <memory>
+namespace amcl {
 
-#include <boost/bind.hpp>
-#include <boost/thread/mutex.hpp>
-
-// Signal handling
-#include <signal.h>
-
-#include "amcl/map/map.h"
-#include "amcl/pf/pf.h"
-#include "amcl/sensors/amcl_odom.h"
-#include "amcl/sensors/amcl_laser.h"
-#include "portable_utils.hpp"
-
-#include "ros/assert.h"
-
-// roscpp
-#include "ros/ros.h"
-
-// Messages that I need
-#include "sensor_msgs/LaserScan.h"
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include "geometry_msgs/PoseArray.h"
-#include "geometry_msgs/Pose.h"
-#include "geometry_msgs/PoseStamped.h"
-#include "nav_msgs/GetMap.h"
-#include "nav_msgs/SetMap.h"
-#include "std_srvs/Empty.h"
-
-// For transform support
-#include "tf2/LinearMath/Transform.h"
-#include "tf2/convert.h"
-#include "tf2/utils.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "tf2_ros/buffer.h"
-#include "tf2_ros/message_filter.h"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2_ros/transform_listener.h"
-#include "message_filters/subscriber.h"
-
-// Dynamic_reconfigure
-#include "dynamic_reconfigure/server.h"
-#include "amcl/AMCLConfig.h"
-
-// Allows AMCL to run from bag file
-#include <rosbag/bag.h>
-#include <rosbag/view.h>
-#include <boost/foreach.hpp>
-
-// For monitoring the estimator
-#include <diagnostic_updater/diagnostic_updater.h>
-
-
-// Custom header files
-#include "common_pkg/Relocalize.h"
-#include "scan_matching/real_time_correlative_scan_matcher_2d.h"
-
-#define NEW_UNIFORM_SAMPLING 1
-
-using namespace amcl;
-
-// Pose hypothesis
-typedef struct
-{
-  // Total weight (weights sum to 1)
-  double weight;
-
-  // Mean of pose esimate
-  pf_vector_t pf_pose_mean;
-
-  // Covariance of pose estimate
-  pf_matrix_t pf_pose_cov;
-
-} amcl_hyp_t;
-
-static double
-normalize(double z)
-{
-  return atan2(sin(z),cos(z));
-}
-static double
-angle_diff(double a, double b)
-{
-  double d1, d2;
-  a = normalize(a);
-  b = normalize(b);
-  d1 = a-b;
-  d2 = 2*M_PI - fabs(d1);
-  if(d1 > 0)
-    d2 *= -1.0;
-  if(fabs(d1) < fabs(d2))
-    return(d1);
-  else
-    return(d2);
-}
-
-static const std::string scan_topic_ = "scan";
-
-/* This function is only useful to have the whole code work
- * with old rosbags that have trailing slashes for their frames
- */
-inline
-std::string stripSlash(const std::string& in)
-{
-  std::string out = in;
-  if ( ( !in.empty() ) && (in[0] == '/') )
-    out.erase(0,1);
-  return out;
-}
-
-class AmclNode
-{
-  public:
-    AmclNode();
-    ~AmclNode();
-
-    /**
-     * @brief Uses TF and LaserScan messages from bag file to drive AMCL instead
-     * @param in_bag_fn input bagfile
-     * @param trigger_global_localization whether to trigger global localization
-     * before starting to process the bagfile
-     */
-    void runFromBag(const std::string &in_bag_fn, bool trigger_global_localization = false);
-
-    int process();
-    void savePoseToServer();
-
-  private:
-    std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
-    std::shared_ptr<tf2_ros::TransformListener> tfl_;
-    std::shared_ptr<tf2_ros::Buffer> tf_;
-
-    bool sent_first_transform_;
-
-    tf2::Transform latest_tf_;
-    bool latest_tf_valid_;
-
-    // Pose-generating function used to uniformly distribute particles over
-    // the map
-    static pf_vector_t uniformPoseGenerator(void* arg);
-#if NEW_UNIFORM_SAMPLING
-    static std::vector<std::pair<int,int> > free_space_indices;
-#endif
-    // Callbacks
-    bool globalLocalizationCallback(std_srvs::Empty::Request& req,
-                                    std_srvs::Empty::Response& res);
-    bool nomotionUpdateCallback(std_srvs::Empty::Request& req,
-                                    std_srvs::Empty::Response& res);
-    bool setMapCallback(nav_msgs::SetMap::Request& req,
-                        nav_msgs::SetMap::Response& res);
-
-    void laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan);
-    void initialPoseReceived(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg);
-    void handleInitialPoseMessage(const geometry_msgs::PoseWithCovarianceStamped& msg);
-    void mapReceived(const nav_msgs::OccupancyGridConstPtr& msg);
-
-    void handleMapMessage(const nav_msgs::OccupancyGrid& msg);
-    void freeMapDependentMemory();
-    map_t* convertMap( const nav_msgs::OccupancyGrid& map_msg );
-    void updatePoseFromServer();
-    void applyInitialPose();
-
-    //parameter for what odom to use
-    std::string odom_frame_id_;
-
-    //paramater to store latest odom pose
-    geometry_msgs::PoseStamped latest_odom_pose_;
-
-    //parameter for what base to use
-    std::string base_frame_id_;
-    std::string global_frame_id_;
-
-    bool use_map_topic_;
-    bool first_map_only_;
-
-    ros::Duration gui_publish_period;
-    ros::Time save_pose_last_time;
-    ros::Duration save_pose_period;
-
-    geometry_msgs::PoseWithCovarianceStamped last_published_pose;
-
-    map_t* map_;
-    char* mapdata;
-    int sx, sy;
-    double resolution;
-
-    message_filters::Subscriber<sensor_msgs::LaserScan>* laser_scan_sub_;
-    tf2_ros::MessageFilter<sensor_msgs::LaserScan>* laser_scan_filter_;
-    ros::Subscriber initial_pose_sub_;
-    std::vector< AMCLLaser* > lasers_;
-    std::vector< bool > lasers_update_;
-    std::map< std::string, int > frame_to_laser_;
-
-    // Particle filter
-    pf_t *pf_;
-    double pf_err_, pf_z_;
-    bool pf_init_;
-    pf_vector_t pf_odom_pose_;
-    double d_thresh_, a_thresh_;
-    int resample_interval_;
-    int resample_count_;
-    double laser_min_range_;
-    double laser_max_range_;
-
-    //Nomotion update control
-    bool m_force_update;  // used to temporarily let amcl update samples even when no motion occurs...
-
-    AMCLOdom* odom_;
-    AMCLLaser* laser_;
-
-    ros::Duration cloud_pub_interval;
-    ros::Time last_cloud_pub_time;
-
-    // For slowing play-back when reading directly from a bag file
-    ros::WallDuration bag_scan_period_;
-
-    void requestMap();
-
-    // Helper to get odometric pose from transform system
-    bool getOdomPose(geometry_msgs::PoseStamped& pose,
-                     double& x, double& y, double& yaw,
-                     const ros::Time& t, const std::string& f);
-
-    //time for tolerance on the published transform,
-    //basically defines how long a map->odom transform is good for
-    ros::Duration transform_tolerance_;
-
-    ros::NodeHandle nh_;
-    ros::NodeHandle private_nh_;
-    ros::Publisher pose_pub_;
-    ros::Publisher particlecloud_pub_;
-    ros::ServiceServer global_loc_srv_;
-    ros::ServiceServer nomotion_update_srv_; //to let amcl update samples without requiring motion
-    ros::ServiceServer set_map_srv_;
-    ros::Subscriber initial_pose_sub_old_;
-    ros::Subscriber map_sub_;
-
-    diagnostic_updater::Updater diagnosic_updater_;
-    void standardDeviationDiagnostics(diagnostic_updater::DiagnosticStatusWrapper& diagnostic_status);
-    double std_warn_level_x_;
-    double std_warn_level_y_;
-    double std_warn_level_yaw_;
-
-    amcl_hyp_t* initial_pose_hyp_;
-    bool first_map_received_;
-    bool first_reconfigure_call_;
-
-    boost::recursive_mutex configuration_mutex_;
-    dynamic_reconfigure::Server<amcl::AMCLConfig> *dsrv_;
-    amcl::AMCLConfig default_config_;
-    ros::Timer check_laser_timer_;
-
-    int max_beams_, min_particles_, max_particles_;
-    double alpha1_, alpha2_, alpha3_, alpha4_, alpha5_;
-    double alpha_slow_, alpha_fast_;
-    double z_hit_, z_short_, z_max_, z_rand_, sigma_hit_, lambda_short_;
-  //beam skip related params
-    bool do_beamskip_;
-    double beam_skip_distance_, beam_skip_threshold_, beam_skip_error_threshold_;
-    double laser_likelihood_max_dist_;
-    odom_model_t odom_model_type_;
-    double init_pose_[3];
-    double init_cov_[3];
-    laser_model_t laser_model_type_;
-    bool tf_broadcast_;
-    bool selective_resampling_;
-
-    void reconfigureCB(amcl::AMCLConfig &config, uint32_t level);
-
-    ros::Time last_laser_received_ts_;
-    ros::Duration laser_check_interval_;
-    void checkLaserReceived(const ros::TimerEvent& event);
-};
-
-std::vector<std::pair<int,int> > AmclNode::free_space_indices;
-
-#define USAGE "USAGE: amcl"
-
-boost::shared_ptr<AmclNode> amcl_node_ptr;
-
-void sigintHandler(int sig)
-{
-  // Save latest pose as we're shutting down.
-  amcl_node_ptr->savePoseToServer();
-  ros::shutdown();
-}
-
-int
-main(int argc, char** argv)
-{
-  ros::init(argc, argv, "amcl");
-  ros::NodeHandle nh;
-
-  // Override default sigint handler
-  signal(SIGINT, sigintHandler);
-
-  // Make our node available to sigintHandler
-  amcl_node_ptr.reset(new AmclNode());
-
-  if (argc == 1)
-  {
-    // run using ROS input
-    ros::spin();
-  }
-  else if ((argc >= 3) && (std::string(argv[1]) == "--run-from-bag"))
-  {
-    if (argc == 3)
-    {
-      amcl_node_ptr->runFromBag(argv[2]);
-    }
-    else if ((argc == 4) && (std::string(argv[3]) == "--global-localization"))
-    {
-      amcl_node_ptr->runFromBag(argv[2], true);
-    }
-  }
-
-  // Without this, our boost locks are not shut down nicely
-  amcl_node_ptr.reset();
-
-  // To quote Morgan, Hooray!
-  return(0);
-}
-
-AmclNode::AmclNode() :
+AmclNode::AmclNode(const proto::RealTimeCorrelativeScanMatcherOptions& options) :
         sent_first_transform_(false),
         latest_tf_valid_(false),
         map_(NULL),
@@ -358,7 +18,8 @@ AmclNode::AmclNode() :
 	      private_nh_("~"),
         initial_pose_hyp_(NULL),
         first_map_received_(false),
-        first_reconfigure_call_(true)
+        first_reconfigure_call_(true),
+        real_time_correlative_scan_matcher_(options)
 {
   boost::recursive_mutex::scoped_lock l(configuration_mutex_);
 
@@ -478,6 +139,8 @@ AmclNode::AmclNode() :
   global_loc_srv_ = nh_.advertiseService("global_localization", 
 					 &AmclNode::globalLocalizationCallback,
                                          this);
+  local_localization_server_ = nh_.advertiseService("local_localization",
+    &AmclNode::HandleLocalLocalization, this);
   nomotion_update_srv_= nh_.advertiseService("request_nomotion_update", &AmclNode::nomotionUpdateCallback, this);
   set_map_srv_= nh_.advertiseService("set_map", &AmclNode::setMapCallback, this);
 
@@ -1082,6 +745,22 @@ AmclNode::uniformPoseGenerator(void* arg)
   return p;
 }
 
+bool AmclNode::HandleLocalLocalization(common_pkg::Relocalize::Request& req,
+    common_pkg::Relocalize::Response& res) {
+  const transform::Rigid2d pose_prediction;
+  transform::Rigid2d initial_ceres_pose = pose_prediction;
+  const sensor::PointCloud filtered_gravity_aligned_point_cloud;
+  if (filtered_gravity_aligned_point_cloud.empty()) {
+    return false;
+  }
+  cartographer::mapping::ValueConversionTables conversion_tables;
+  auto grid = cartographer::io::CreateProbabilityGrid(0.5, &conversion_tables);
+  const double score = real_time_correlative_scan_matcher_.Match(
+        pose_prediction, filtered_gravity_aligned_point_cloud,
+        grid, &initial_ceres_pose);
+  return true;
+}
+
 bool
 AmclNode::globalLocalizationCallback(std_srvs::Empty::Request& req,
                                      std_srvs::Empty::Response& res)
@@ -1636,3 +1315,5 @@ AmclNode::standardDeviationDiagnostics(diagnostic_updater::DiagnosticStatusWrapp
     diagnostic_status.summary(diagnostic_msgs::DiagnosticStatus::OK, "OK");
   }
 }
+
+} // namespace amcl
---
+++ amcl/src/amcl_node.cc
@@ -0,0 +1,31 @@
+#include <memory>
+#include <ros/ros.h>
+
+#include "amcl.h"
+#include "cartographer/mapping/proto/scan_matching/real_time_correlative_scan_matcher_options.pb.h"
+
+using namespace cartographer::mapping::scan_matching;
+
+constexpr double PI = 3.141592653;
+std::vector<std::pair<int,int>> amcl::AmclNode::free_space_indices; // initialize static class member variable
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "amcl");
+  ros::NodeHandle nh;
+
+  proto::RealTimeCorrelativeScanMatcherOptions options; // TODO: a function for reading params from a file
+  options.set_linear_search_window(0.1);
+  options.set_angular_search_window(20./180.*PI);
+  options.set_translation_delta_cost_weight(1e-1);
+  options.set_rotation_delta_cost_weight(1e-1);
+
+  std::shared_ptr<amcl::AmclNode> amcl_node_ptr = std::make_shared<amcl::AmclNode>(options);
+
+  ros::Rate rate{50};
+  while (ros::ok()) {
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
